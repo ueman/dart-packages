576037047514bd363bb0462b720295c56d155d98
Jonas Uekötter <ueman@users.noreply.github.com>
2025-07-06 10:19:05 +0200

    Dynamic Island Watermark (#7)

---
+++ dynamic_island_watermark/README.md
@@ -0,0 +1,69 @@
+# dynamic_island_watermark
+
+<p align="center">
+  <a href="https://pub.dev/packages/dynamic_island_watermark"><img src="https://img.shields.io/pub/v/dynamic_island_watermark.svg" alt="pub.dev"></a>
+  <a href="https://github.com/ueman#sponsor-me"><img src="https://img.shields.io/github/sponsors/ueman" alt="Sponsoring"></a>
+  <a href="https://pub.dev/packages/dynamic_island_watermark/score"><img src="https://img.shields.io/pub/likes/dynamic_island_watermark" alt="likes"></a>
+  <a href="https://pub.dev/packages/dynamic_island_watermark/score"><img src="https://img.shields.io/pub/points/dynamic_island_watermark" alt="pub points"></a>
+</p>
+
+A Flutter package that adds a subtle watermark on screenshots behind the Dynamic Island on supported iPhone models. Perfect for growth-hacking or adding helpful information in screenshots.
+
+## Motivation
+
+The Dynamic Island on iPhone models like the Pro series is a unique, interactive UI feature. Adding a watermark behind the Dynamic Island helps you:
+
+* **Growth hack**: A/B test new features or promotional content without permanently modifying your UI.
+* **Debugging**: Include version information in screenshot that get sent to you, without making it otherwise user visible
+
+## Installation
+
+Add `dynamic_island_watermark` as a dependency in your `pubspec.yaml`:
+
+```yaml
+dependencies:
+  dynamic_island_watermark: ^1.0.0
+```
+
+Then fetch the package:
+
+```bash
+flutter pub get
+```
+
+## Usage
+
+Wrap your app with the `DynamicIslandWatermark` widget:
+
+```dart
+import 'package:dynamic_island_watermark/dynamic_island_watermark.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    DynamicIslandWatermark(
+      watermark: Watermark(),
+      child: const MyApp(),
+    ),
+  );
+}
+```
+
+### Parameters
+
+* `watermark` (Widget): The widget displayed as the watermark.
+* `child` (Widget): Your app.
+
+## Demo
+
+<video controls>
+  <source src="https://github.com/ueman/dart-packages/raw/213a8b0d7b6c5aecf48db9749f81b105fb0f8b31/dynamic_island_watermark/media/example.mov" type="video/mp4">
+</video>
+
+## Contributing
+
+Contributions, issues, and feature requests are welcome! Feel free to open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
---
+++ dynamic_island_watermark/lib/dynamic_island_watermark.dart
@@ -0,0 +1,160 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class DynamicIslandWatermark extends StatelessWidget {
+  /// Typically this is your Material app
+  final Widget child;
+
+  /// The watermark
+  final Widget watermark;
+
+  /// The text direction for this subtree.
+  final TextDirection? textDirection;
+
+  const DynamicIslandWatermark({
+    super.key,
+    required this.watermark,
+    required this.child,
+    this.textDirection,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (kIsWeb) {
+      return child;
+    }
+    if (!Platform.isIOS) {
+      return child;
+    }
+
+    final actualTextDirection =
+        textDirection ??
+        Directionality.maybeOf(context) ??
+        _getDeviceDefault(context);
+
+    return Stack(
+      textDirection: actualTextDirection,
+      children: [
+        child,
+        _IslandWatermark(
+          textDirection: actualTextDirection,
+          watermark: watermark,
+        ),
+      ],
+    );
+  }
+
+  TextDirection _getDeviceDefault(BuildContext context) {
+    final locale =
+        View.maybeOf(context)?.platformDispatcher.locale ??
+        PlatformDispatcher.instance.locale;
+    const rtlLanguages = ['ar', 'fa', 'he', 'ps', 'sd', 'ur'];
+    return rtlLanguages.contains(locale.languageCode.toLowerCase())
+        ? TextDirection.rtl
+        : TextDirection.ltr;
+  }
+}
+
+class _IslandWatermark extends StatefulWidget {
+  /// The text direction for this subtree.
+  final TextDirection textDirection;
+
+  final Widget watermark;
+
+  const _IslandWatermark({
+    required this.textDirection,
+    required this.watermark,
+  });
+
+  @override
+  State<_IslandWatermark> createState() => _IslandWatermarkState();
+}
+
+class _IslandWatermarkState extends State<_IslandWatermark> {
+  String? modelName;
+
+  @override
+  void initState() {
+    super.initState();
+    unawaited(_loadiPhoneModel());
+  }
+
+  Future<void> _loadiPhoneModel() async {
+    final name = (await DeviceInfoPlugin().iosInfo).modelName;
+    if (!mounted) return;
+    setState(() {
+      modelName = name;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (modelName == null) return SizedBox.shrink();
+
+    final config = _getConfig(modelName!);
+
+    if (config == null) return SizedBox.shrink();
+
+    return Positioned(
+      top: config.top,
+      height: config.height,
+      left: config.horizontal,
+      right: config.horizontal,
+      // TODO(ueman): Use ClipRSuperellipse eventually?
+      child: ClipRRect(
+        //clipBehavior: Clip.none, // Can be enabled/disbled for better testing
+        borderRadius: BorderRadius.all(Radius.circular(config.borderRadius)),
+        child: Directionality(
+          textDirection: widget.textDirection,
+          child: widget.watermark,
+        ),
+      ),
+    );
+  }
+
+  _DynamicIslandConfig? _getConfig(String modelName) {
+    return switch (modelName) {
+      'iPhone 16 Pro' => _DynamicIslandConfig.iPhone16Pro,
+      'iPhone 16 Pro Max' => _DynamicIslandConfig.iPhone16ProMax,
+      'iPhone 16' => _DynamicIslandConfig.iPhone16,
+      'iPhone 16 Plus' => _DynamicIslandConfig.iPhone16Plus,
+      'iPhone 15 Pro' => _DynamicIslandConfig.iPhone15Pro,
+      'iPhone 15 Pro Max' => _DynamicIslandConfig.iPhone15ProMax,
+      'iPhone 15' => _DynamicIslandConfig.iPhone15,
+      'iPhone 15 Plus' => _DynamicIslandConfig.iPhone15Plus,
+      'iPhone 14 Pro' => _DynamicIslandConfig.iPhone15Pro,
+      'iPhone 14 Pro Max' => _DynamicIslandConfig.iPhone15ProMax,
+      _ => null,
+    };
+  }
+}
+
+enum _DynamicIslandConfig {
+  iPhone16(top: 12, height: 36, horizontal: 134, borderRadius: 20),
+  iPhone16Plus(top: 12, height: 36, horizontal: 153, borderRadius: 20),
+  iPhone16Pro(top: 14, height: 36, horizontal: 139, borderRadius: 25),
+  iPhone16ProMax(top: 14, height: 36, horizontal: 158, borderRadius: 25),
+  iPhone15(top: 12, height: 35, horizontal: 135, borderRadius: 20),
+  iPhone15Plus(top: 12, height: 35, horizontal: 153, borderRadius: 20),
+  // Also used for iPhone 14 Pro
+  iPhone15Pro(top: 12, height: 35, horizontal: 135, borderRadius: 25),
+  // Also used for iPhone 14 Pro Max
+  iPhone15ProMax(top: 12, height: 35, horizontal: 153, borderRadius: 25);
+
+  final double top;
+  final double height;
+  final double horizontal;
+  final double borderRadius;
+
+  const _DynamicIslandConfig({
+    required this.top,
+    required this.height,
+    required this.horizontal,
+    required this.borderRadius,
+  });
+}
